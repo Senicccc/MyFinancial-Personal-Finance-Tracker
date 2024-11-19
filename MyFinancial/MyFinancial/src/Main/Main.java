fe7b5211aa936ad4ad61ff6343fcaf31924c34c0
Nicholas Jeremy <145427187+Senicccc@users.noreply.github.com>
2024-11-19 20:13:05 +0700

    Add files via upload

---
+++ MyFinancial/MyFinancial/src/Main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu1 MenuPertama = new Menu1();
+        MenuPertama.setVisible(true);
+        MenuPertama.pack();
+        MenuPertama.setLocationRelativeTo(null);
+    }
+    
+}
